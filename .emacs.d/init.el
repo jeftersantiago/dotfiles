6fac1522de9793a29193687a40e1b393c4eaf8a8
jefterrsantiago <jefterrsantiago@gmail.com>
2021-02-03 02:38:25 -0300

    lots of emacs changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,15 +1,13 @@
 ;; load the settings file - whithout dependences
-(when( >= emacs-major-version 24)
-  (require 'package)
-  (add-to-list
-   'package-archives
-   '("MELPA" . "https://melpa.org/packages/") t))
-
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
+
 `use-package'
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
-(put 'downcase-region 'disabled nil)
