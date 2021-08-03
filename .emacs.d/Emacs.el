38a0355489803f4349ed0a3ecb1f762b11490c80
jefter66 <jeftersantiago@protonmail.com>
2021-08-03 02:40:42 -0300

    don't know

--- .emacs.d/Emacs.el
+++ .emacs.d/Emacs.el
@@ -249,35 +249,46 @@
 (global-set-key (kbd "C-x p") 'org-latex-export-to-pdf)
 
 (use-package auctex
-  :ensure t
-  :hook ((latex-mode LaTeX-mode) . tex)
-  :config
-  (font-lock-mode)
-  (add-to-list 'font-latex-math-environments "dmath"))
+      :ensure t
+      :hook ((latex-mode LaTeX-mode) . tex)
+      :config
+      (font-lock-mode)
+      (add-to-list 'font-latex-math-environments "dmath"))
 
-(add-hook 'LaTeX-mode-hook 'TeX-mode)
-(add-hook 'LaTeX-mode-hook 'font-lock-mode)
+    (add-hook 'LaTeX-mode-hook 'TeX-mode)
+    (add-hook 'LaTeX-mode-hook 'font-lock-mode)
 
 
-(add-hook 'LaTeX-mode-hook 'visual-line-mode)
-(add-hook 'LaTeX-mode-hook 'flyspell-mode)
-(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+    (add-hook 'LaTeX-mode-hook 'visual-line-mode)
+    (add-hook 'LaTeX-mode-hook 'flyspell-mode)
+    (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+    (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 
-(setq reftex-plug-into-AUCTeX t)
+    (setq reftex-plug-into-AUCTeX t)
 
-(setq TeX-auto-save t)
-(setq TeX-parse-self t)
-(setq TeX-save-query t)
-(setq-default TeX-master nil)
-(setq TeX-PDF-mode t)
-                                        ; (add-hook 'LateX-mode-hook (lambda () (latex-preview-pane-mode)))
-                                        ; (global-set-key (kbd "C-x l ") 'latex-preview-pane-mode)
-(global-set-key (kbd "C-x l ") 'pdflatex)
-(add-to-list 'org-latex-packages-alist '("" "listings" nil))
-(setq org-latex-listings t)   
-(setq org-latex-listings-options '(("breaklines" "true")))
+    (setq TeX-auto-save t)
+    (setq TeX-parse-self t)
+    (setq TeX-save-query t)
+    (setq-default TeX-master nil)
+    (setq TeX-PDF-mode t)
+                                            ; (add-hook 'LateX-mode-hook (lambda () (latex-preview-pane-mode)))
+                                            ; (global-set-key (kbd "C-x l ") 'latex-preview-pane-mode)
+;    (global-set-key (kbd "C-x l ") 'pdflatex)
+    (add-to-list 'org-latex-packages-alist '("" "listings" nil))
+    (setq org-latex-listings t)   
+    (setq org-latex-listings-options '(("breaklines" "true")))
+
+
+    (setq latex-run-command "pdflatex")
+
+    (defun hrs/mark-done-and-archive ()
+      " Mark the state of an org-mode item as DONE and archive it."
+      (interactive)
+      (org-todo 'done)
+      (org-archive-subtree))
+    (global-set-key (kbd "C-c C-x C-s") 'hrs/mark-done-and-archive)
+    (setq org-log-done 'time)
 
 (use-package dired-sidebar
   :ensure t
